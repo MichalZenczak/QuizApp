81b2cfb4ca08e18a69630c4eedd6d529db716c5b
Michal <zenczak.michal@gmail.com>
2017-12-14 23:28:44 +0100

    Updated checkAnswers and createMessage methods

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -21,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
         submitAnswerButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
 
-                int[] correctAnswers = checkAnswers();
+                int[] results = checkAnswers();
 
-                Toast.makeText(getApplicationContext(), createMessage(correctAnswers, totalQuestions), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), createMessage(results, totalQuestions), Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * This method checks the correct answers and calculates the score.
+     * This method checks the correct answers and calculates the result.
      */
     public int[] checkAnswers() {
             int correctAnswers = 0;
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             int isQuestionOneChecked = questionOneAnswers.getCheckedRadioButtonId();
             if (isQuestionOneChecked == -1){
                 questionsNotChecked += 1;
-            }else{
+            }else {
                 RadioButton questionOneCorrectAnswer = findViewById(R.id.question_1_answer_1_correct);
                 Boolean isQuestionOneCorrect = questionOneCorrectAnswer.isChecked();
                 if (isQuestionOneCorrect){
@@ -51,40 +51,73 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-
-            RadioButton secondQuestion = findViewById(R.id.question_2_answer_4_correct);
-            Boolean isSecondQuestionCorrect = secondQuestion.isChecked();
-            if (isSecondQuestionCorrect){
-                correctAnswers += 1;
+            RadioGroup questionTwoAnswers = findViewById(R.id.radio_group_question_2);
+            int isQuestionTwoChecked = questionTwoAnswers.getCheckedRadioButtonId();
+            if (isQuestionTwoChecked == -1){
+                questionsNotChecked += 1;
+            }else {
+                RadioButton questionTwoCorrectAnswer = findViewById(R.id.question_2_answer_4_correct);
+                Boolean isQuestionTwoCorrect = questionTwoCorrectAnswer.isChecked();
+                if (isQuestionTwoCorrect){
+                    correctAnswers += 1;
+                }
             }
 
-            RadioButton thirdQuestion = findViewById(R.id.question_3_answer_3_correct);
-            Boolean isThirdQuestionCorrect = thirdQuestion.isChecked();
-            if (isThirdQuestionCorrect){
-                correctAnswers += 1;
+            RadioGroup questionThreeAnswers = findViewById(R.id.radio_group_question_3);
+            int isQuestionThreeChecked = questionThreeAnswers.getCheckedRadioButtonId();
+            if (isQuestionThreeChecked == -1){
+                questionsNotChecked += 1;
+            }else {
+                RadioButton questionThreeCorrectAnswer = findViewById(R.id.question_3_answer_3_correct);
+                Boolean isQuestionThreeCorrect = questionThreeCorrectAnswer.isChecked();
+                if (isQuestionThreeCorrect){
+                    correctAnswers += 1;
+                }
             }
 
-            RadioButton fourthQuestion = findViewById(R.id.question_4_answer_2_correct);
-            Boolean isFourthQuestionCorrect = fourthQuestion.isChecked();
-            if (isFourthQuestionCorrect){
-                correctAnswers += 1;
+            RadioGroup questionFourAnswers = findViewById(R.id.radio_group_question_4);
+            int isQuestionFourChecked = questionFourAnswers.getCheckedRadioButtonId();
+            if (isQuestionFourChecked == -1){
+                questionsNotChecked += 1;
+            }else {
+                RadioButton questionFourCorrectAnswer = findViewById(R.id.question_4_answer_2_correct);
+                Boolean isQuestionFourCorrect = questionFourCorrectAnswer.isChecked();
+                if (isQuestionFourCorrect){
+                    correctAnswers += 1;
+                }
             }
 
-            RadioButton fifthQuestion = findViewById(R.id.question_5_answer_4_correct);
-            Boolean isFifthQuestionCorrect = fifthQuestion.isChecked();
-            if (isFifthQuestionCorrect){
-                correctAnswers += 1;
+            RadioGroup questionFiveAnswers = findViewById(R.id.radio_group_question_5);
+            int isQuestionFiveCheceked = questionFiveAnswers.getCheckedRadioButtonId();
+            if (isQuestionFiveCheceked == -1 ){
+                questionsNotChecked += 1;
+            }else {
+                RadioButton questionFiveCorrectAnswer = findViewById(R.id.question_5_answer_4_correct);
+                Boolean isQuestionFiveCorrect = questionFiveCorrectAnswer.isChecked();
+                if (isQuestionFiveCorrect){
+                    correctAnswers += 1;
+                }
             }
 
             return new int[] {correctAnswers, questionsNotChecked};
     }
 
-    public String createMessage(int[] correctAnswers, int totalQuestions){
-        String message = getString(R.string.score_message_1) + " " + correctAnswers[0];
-        message += "\n" + getString(R.string.score_message_2) + " " + totalQuestions;
-        message += "\n" + getString(R.string.score_message_3);
-        message += "\n" + correctAnswers[1];
-        return message;
+    public String createMessage(int[] results, int totalQuestions){
+        String message = "";
+
+        if (results[1] > 0){
+            message += getString(R.string.question_not_answered) + " " + results[1] + " " + getString(R.string.score_message_3);
+            return message;
+        }else if (results[0] == totalQuestions){
+            message += getString(R.string.perfect_score);
+            return message;
+        }else {
+            message += getString(R.string.score_message_1) + " " + results[0];
+            message += " " + getString(R.string.score_message_2) + " " + totalQuestions;
+            message += "\n" + getString(R.string.score_message_3);
+            return message;
+        }
+
     }
 
 
